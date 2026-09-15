62f530b54c3c467d7b916cd97a9f9219fea78e64
hongphatde <hongphat.de@gmail.com>
2026-09-15 13:42:52 +0700

    feat: add 3 exercise of codewars 15/09/2026

--- String/string01.py
+++ String/string01.py
@@ -75,11 +75,96 @@ print(count_sheep([True,  True,  True,  False,
   True,  True,  True,  True ,
   False, False, True,  True]))
 """
-# Exercise 5: Sum Mixed Array
+# Exercise 5: Sum Mixed integer Array
 def sum_mix(arr):
+# Solution 1
     result = 0
     for number in arr:
         result += int(number)
     return result
-        
-print(sum_mix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']))
+# Solution 2
+    #return sum([int(i) for i in arr])
+# Test case        
+#print(sum_mix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']))
+
+#Exercise 6: List Filtering 
+def filter_list(exercise):
+# Solution 1
+
+    list_new = []
+    
+    for element in exercise:
+        # Check if element is an integer. 
+        if isinstance(element, int):
+        # Add element list new
+            list_new.append(element)
+    return list_new
+
+# Solution 2: Shorty
+    #return [element for element in exercise if isinstance(element, int)]
+# Test case
+"""
+print(filter_list([1,2,'aasf','1','123',123]))
+print(filter_list([1,'a','b',0,15]))
+print(filter_list([1,'a','b',0,15]))
+
+"""
+# Exercise 7: Reversed Words
+def reverse_words(sentence):
+# Solution 1
+    # Reverse the characters in sentence
+    sentence = sentence[::-1]
+    # Receive the processed words
+    result =""
+    # Receive the characters that have passed
+    word = ""
+    
+    for char in sentence:
+        if char == " ":
+        # Enter each word if a space is encountered.
+            result += word[::-1] + " "
+        # Reset word
+            word = ""
+        else:
+            word += char
+    # Enter the last word when there are no spaces.
+    result += word[::-1]
+    return result
+
+# Solution 2
+"""
+    sentence_list = sentence.split()
+    return " ".join(sentence_list[::-1])
+"""
+
+# Test case
+print(reverse_words("The greatest victory is that which requires no battle"))
+
+# Exercise 8: Beginner Series #4 Cockroach
+   # Exercise: exchange km/h to cm/s of Cockroach's speed
+def cockroach_speech(speed):
+    return speed // 0.036
+
+# Test case
+print(cockroach_speech(1.08))
+print(cockroach_speech(2.7781687862099695))
+print(cockroach_speech(0.9353327640357478))
+
+# Exercise 9: Remove the minimum
+def remove_minimum(numbers):
+    # Return null list, if list don't have value
+    if not numbers:
+        return []
+   
+    minimun_value = numbers[0]
+    minimun_index = 0
+    # Loop numbers list with iterable, number 
+    for iterable, number in enumerate(numbers):
+        if number < minimun_value:
+            minimun_value = number
+            minimun_index = iterable
+    return numbers[:minimun_index] + numbers[minimun_index + 1:]
+
+print(remove_minimum([128, 374, 67, 398, 146, 60]))
+print(remove_minimum([304,153, 262, 28]))
+print(remove_minimum([192, 312]))
